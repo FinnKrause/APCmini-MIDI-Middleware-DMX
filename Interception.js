90a334a65e3265af08bcfd0583a27f1c84af91f5
FinnKrause <finn_krause@gmx.de>
2025-01-06 02:47:55 +0100

    clarity

--- Interception.js
+++ Interception.js
@@ -2,30 +2,24 @@ const easymidi = require("easymidi");
 const { Colors, LModes } = require("./constants.js");
 const APCAPI = require("./APCAPI.js");
 
-const mk2output = new easymidi.Output("APC mini mk2");
-const input = new easymidi.Input("APC mini mk2");
+const toAPC = new easymidi.Output("APC mini mk2");
+const fromAPC = new easymidi.Input("APC mini mk2");
 const toJoker = new easymidi.Output("JokerIn");
 const fromJoker = new easymidi.Input("JokerOut");
 
-input.on("noteon", function (msg) {
-  // api.toggleNote(msg.note);
-  toJoker.send("noteon", {
-    ...msg,
-  });
-});
+const APCmini = new APCAPI(toAPC);
 
-//Fader routing
-input.on("cc", (msg) => {
-  toJoker.send("cc", msg);
-});
+// Forwarding all traffic
+// Direction: APC mini --> DMX Joker
+fromAPC.on("noteon", () => toJoker.send("noteon", { ...msg }));
+fromAPC.on("cc", (msg) => toJoker.send("cc", msg));
 
+// Handling button state changes
+// Direction: DMX Joker --> APC mini
 fromJoker.on("noteon", (msg) => {
-  // console.log("From Joker: " + JSON.stringify(msg));
-
-  if (msg.velocity == 64) api.setButtonLook(msg.note, "on");
-  else api.setButtonLook(msg.note, "off");
+  if (msg.velocity == 64) APCmini.setButtonLook(msg.note, "on");
+  else APCmini.setButtonLook(msg.note, "off");
 });
-
-const api = new APCAPI(mk2output);
-api.forEachMatrix((i) => api.setButtonLook(i, "off"));
-api.forEachScene((i) => api.setButtonLook(i, "off"));
+W;
+APCmini.forEachMatrix((i) => APCmini.setButtonLook(i, "off"));
+APCmini.forEachScene((i) => APCmini.setButtonLook(i, "off"));
